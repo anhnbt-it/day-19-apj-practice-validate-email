e6e908be9174370506349aa1e98c673f08baaf7a
anhnbt-it <anhnbt.it@gmail.com>
2020-10-29 09:58:48 +0700

    feat: add ValidateEmailExampleTest.java

---
+++ test/ValidateEmailExampleTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AnhNBT (anhnbt.it@gmail.com)
+ * Date: 10/29/2020
+ * Time: 9:53 AM
+ */
+class ValidateEmailExampleTest {
+
+    private ValidateEmailExample emailValidate = new ValidateEmailExample();
+
+    @Test
+    @DisplayName("a@gmail.com")
+    void testA() {
+        String regex = "a@gmail.com";
+        boolean expected = true;
+        boolean actual = emailValidate.validate(regex);
+        assertEquals(expected, actual);
+    }
+
+}
